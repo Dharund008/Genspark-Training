41c2bb9c35c9081987ec2a2790bc8cee53e6ff74
dharund008 <dharund0810@gmail.com>
2025-05-23 17:46:50 +0530

    file updated

--- May22/taskfile/OCP/IInterestCalculator.cs
+++ May22/taskfile/OCP/IInterestCalculator.cs
@@ -3,7 +3,7 @@ using System;
 namespace OCP.solid
 {
     public interface IInterestCalculator
-    {//interface for calculation the bonus, so that easy to use whenever required 
+    { 
         double CalculateInterest(double principal);
     }
 }
