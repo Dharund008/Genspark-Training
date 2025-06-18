c17b55257e1c3a36594f0bfa71501eae488e98db
dharund008 <dharund0810@gmail.com>
2025-06-18 15:57:55 +0530

    filter functionality -gender,role,state

--- June18/user-dashboard/src/app/dashboard/dashboard.ts
+++ June18/user-dashboard/src/app/dashboard/dashboard.ts
@@ -12,6 +12,9 @@ import { UserService } from '../services/UserService';
 export class Dashboard implements OnInit {
 
   Users: User[] = []; //stors list of users.
+  filteredUsers: User[] = []; // Stores filtered list
+  // searchGender: string = "";
+  searchText: string = "";
 
   constructor(private userService: UserService) {} // Injecting UserService to handle API calls
 
@@ -21,12 +24,36 @@ export class Dashboard implements OnInit {
       {
         console.log('Fetched Users:', data);
         this.Users = data.users as User[];
+        this.filteredUsers = [...this.Users]; //initialising it so it would be filtered starting from the beginning
       },
       error: (error: any) =>
       {
         console.error('Error fetching users:', error);
       }
     });
+  }
+
+  filterUsers()
+  {
+      //this.searchGender = this.searchGender.toLowerCase();
+      this.filteredUsers = this.Users.filter(user =>{
+      // return user.gender.toLowerCase() == (this.searchGender);
+      if (this.searchText.toLowerCase().includes(user.role.toLowerCase())){
+        return true;
+      }
+      else if(this.searchText.toLowerCase().includes(user.address.state.toLowerCase()))
+      {
+        return true;
+      }
+      else if(this.searchText.toLowerCase() == (user.gender.toLowerCase()))
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    })
   }
  
 
